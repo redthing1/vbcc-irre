5513721c91d4249629e613f189d74f0b0bb8e4fd
xdrie <xdrie@alt.icu>
2020-12-02 22:18:22 -0800

    r27 = pc

--- machines/irre/machine.c
+++ machines/irre/machine.c
@@ -68,7 +68,7 @@ char *regnames[MAXR+1]={"noreg",
                         "r0","r1","r2","r3","r4","r5","r6","r7",
                         "r8","r9","r10","r11","r12","r13","r14","r15",
                         "r16","r17","r18","r19","r20","r21","r22","r23",
-                        "r24","r25","r26","r27","lr","ad","at","sp"};
+                        "r24","r25","r26","pc","lr","ad","at","sp"};
 
 /*  The Size of each register in bytes.                         */
 zmax regsize[MAXR+1];
