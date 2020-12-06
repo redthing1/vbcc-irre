e6a6e11b004b0f58dbf4e5a666f5dab63bde4815
xdrie <xdrie@alt.icu>
2020-12-06 02:28:10 +0000

    add directivea for section names

--- machines/irre/machine.c
+++ machines/irre/machine.c
@@ -138,7 +138,7 @@ static char *udt[MAX_TYPE + 1] = {"??", "uc", "us", "ui", "ul", "ull", "f", "d",
 // static long stack;
 // static int stack_valid;
 static int section = -1, newobj;
-static char *codename = "\t; .text\n", *dataname = "\t; .data\n", *bssname = "", *rodataname = "\t.section\t.rodata\n";
+static char *codename = "%%section\ttext\n", *dataname = "%%section\tdata\n", *bssname = "%%section\tbss\n", *rodataname = "%%section\trodata\n";
 
 /* return-instruction */
 static char *ret = "\tret\n";
